8e6b21195f85cef3321235efd20ebb67ce7361db
Stephen Szpak <srszpak@gmail.com>
2016-08-08 22:10:21 -0500

    added final

---
+++ reindeer.js
@@ -0,0 +1,46 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement =  document.getElementById("coloredReindeer")
+
+
+for (var i = 0; i < reindeer.length; i++) {
+  console.log(colors[i] + reindeer[i])
+   hohohoElement.innerHTML += "<div>" + colors[i] + " "+ reindeer[i] + "</div>";
+}
+
+
+//h!!! USE HTML TAGS FOR STRINGS !!!!!
+
+
+// for (var i = 0; i < reindeer.length; i++) {
+	
+// 	// for (var i = 0; i < colors.length; i++) {
+// 	// 	colors[i] + reindeer[i];
+// 	console.log(colors[i] + reindeer[i]);
+// };
+
+// var hohohoElement =  document.getElementById("coloredReindeer")
+
+// console.log(hohohoElement);
+
+// for (var i = 0; i < colors.length; i++) {
+// 	colors[i]
+// 	// } if (i === reindeer.length){
+// 		console.log(colors[i] + reindeer[i]);
+// 	};
+
+// var additon = (colors[i] + reindeer[i]);
+
+// console.log(colors[0] + reindeer[0])
+// console.log(colors[1] + reindeer[1])
+// console.log(colors[2] + reindeer[2])
+// console.log(colors[3] + reindeer[3])
+// console.log(colors[4] + reindeer[4])
+// console.log(colors[5] + reindeer[5])
+// console.log(colors[6] + reindeer[6])
+// console.log(colors[7] + reindeer[7])
+
+
+
+
